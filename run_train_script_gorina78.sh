c4165ee701d50ae71153221b50c31b376b79fdfd
derui <derui>
2024-05-15 13:55:40 +0000

    modify the running script

--- run_train_script_gorina78.sh
+++ run_train_script_gorina78.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodelist=gorina9
 #SBATCH --partition=gpuA100
 #SBATCH --time=29:59:59
-#SBATCH --job-name=llm_fine_tuning_commonsense_170k
-#SBATCH --output=llm_fine_tuning_commonsense_170k.out
+#SBATCH --job-name=alpaca-lora
+#SBATCH --output=llm_fine_tuning_alpaca_training_lora.out
 
 #conda init bash
 uenv verbose cuda-11.4 cudnn-11.4-8.2.4
@@ -13,11 +13,11 @@ conda activate base
 
 python finetune.py \
   --base_model 'yahma/llama-7b-hf' \
-  --data_path 'ft-training_set/commonsense_170k.json' \
-  --output_dir './trained_models/llama-lora' \
-  --batch_size 36 \
-  --micro_batch_size 12 \
-  --num_epochs 1 \
+  --data_path 'dataset/alpaca/train.json' \
+  --output_dir './trained_models/alpaca-training-lora' \
+  --batch_size 64 \
+  --micro_batch_size 16 \
+  --num_epochs 3 \
   --learning_rate 3e-4 \
   --cutoff_len 256 \
   --val_set_size 120 \
