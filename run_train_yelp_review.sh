f244d32ed50e3a4095e2e7506f5bdc7f0aaa43c9
jiahuigeng <jiahui.geng@rwth-aachen.de>
2024-05-16 19:14:47 +0200

    change the running script in server

--- run_train_yelp_review.sh
+++ run_train_yelp_review.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodelist=gorina9
 #SBATCH --partition=gpuA100
 #SBATCH --time=29:59:59
-#SBATCH --job-name=alpaca-lora
-#SBATCH --output=llm_fine_tuning_alpaca_training_lora_wotqdm.out
+#SBATCH --job-name=alpaca-lora-yelp_review
+#SBATCH --output=llm_lora_alpaca_yelp_review.out
 
 #conda init bash
 uenv verbose cuda-11.4 cudnn-11.4-8.2.4
@@ -13,11 +13,11 @@ conda activate base
 
 python finetune.py \
   --base_model 'yahma/llama-7b-hf' \
-  --data_path 'dataset/alpaca/train.json' \
-  --output_dir './trained_models/alpaca-training-lora' \
+  --data_path 'dataset/yelp_review_full/train.json' \
+  --output_dir './trained_models/llama7b-lora-yelp_review' \
   --batch_size 64 \
   --micro_batch_size 32 \
-  --num_epochs 3 \
+  --num_epochs 1 \
   --learning_rate 3e-4 \
   --cutoff_len 256 \
   --val_set_size 120 \
