efce3aa3be6f2016ab04fd2cc3f74fcd6d8de3b0
jiahuigeng <jiahui.geng@rwth-aachen.de>
2024-06-02 00:15:46 +0200

    debug

--- mia-attack/mimir/run.sh
+++ mia-attack/mimir/run.sh
@@ -1,3 +1,3 @@
 export MIMIR_CACHE_PATH="./cache"
 export MIMIR_DATA_SOURCE="./data_source"
-CUDA_VISIBLE_DEVICES=5,6 python run.py --config configs/mi_test.json
+CUDA_VISIBLE_DEVICES=5,6 python run.py --config configs/mi.json
